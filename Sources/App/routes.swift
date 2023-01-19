0f4f4d4d60fa0e26af23f1a9c649555575e2078d
Raffaele S <coverforhobbyit@gmail.com>
2023-01-19 10:21:31 +0100

    Added the drinks.json to the resources

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -16,17 +16,12 @@ func routes(_ app: Application) throws {
         "Ciao!"
     }
     
-    app.get("mostra-json") { req -> EventLoopFuture<Response> in
-        // Add the json in /Users/[USERNAME]/Library/Developer/Xcode/DerivedData/drinkAPI-epmecbgmahbtrndsjzlnjhocxivl/Build/Products/Debug/Public/[JSON HERE]
-        
-        let filepath = app.directory.publicDirectory + "drinks.json"
-        let newPath = FileManager.default.currentDirectoryPath + "/drinks.json"
-        print(newPath)
-        print("Sono qua: \(newPath)")
-        guard FileManager.default.fileExists(atPath: newPath) else {
-            throw Abort(.notFound)
+    app.get("drinksJson") { req -> EventLoopFuture<Response> in
+        guard let file = Bundle.module.url(forResource: "drinks", withExtension: "json")
+        else {
+            fatalError("Couldn't find the file")
         }
-        let jsonData = try Data(contentsOf: URL(fileURLWithPath: filepath))
+        let jsonData = try Data(contentsOf: file)
         let json = try JSONDecoder().decode([Drink].self, from: jsonData)
         let jsonString = String(data: try JSONEncoder().encode(json), encoding: .utf8)
         var buffer = ByteBuffer.init()
@@ -37,15 +32,6 @@ func routes(_ app: Application) throws {
     }
    
     
-    // http://127.0.0.1:8080/mostra-json
-//    app.get("mostra-json") { req -> EventLoopFuture<Response> in
-//        let jsonData = try Data(contentsOf: URL(fileURLWithPath: "drinks.json"))
-//        let json = try JSONDecoder().decode([Drink].self, from: jsonData)
-//        let jsonString = String(data: try JSONEncoder().encode(json), encoding: .utf8)
-//        var buffer = ByteBuffer.init()
-//        buffer.writeString(jsonString ?? "")
-//        let response = Response(headers: HTTPHeaders(), body: .init(buffer: buffer))
-//        response.headers.contentType = .json
-//        return req.eventLoop.makeSucceededFuture(response)
-//    }
+    // http://127.0.0.1:8080/drinksJson
+
 }
